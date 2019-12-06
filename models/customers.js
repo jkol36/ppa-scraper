a1a6d2bb365b2bf5693cc2a09e9e0aa6ec9243fc
Jon Kolman <jonkolman@Jons-MacBook-Pro.local>
2019-12-06 15:56:23 -0500

    models

---
+++ models/customers.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+
+
+const customerSchema = mongoose.Schema({
+	id: Number,
+	created_at: String,
+	updated_at: String,
+}, {strict:false})
+
+
+export default mongoose.model('customers', customerSchema)
