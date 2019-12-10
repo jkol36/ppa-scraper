9b1a2f81c5884a959bce107baf5c1c881b7ca400
Jon Kolman <jonathankolman@gmail.com>
2019-12-10 10:25:38 -0500

    promisify actions

--- actions/index.js
+++ actions/index.js
@@ -2,10 +2,12 @@ import { Mongoose } from "mongoose"
 import * as C from '../constants'
 
 export const changePage = (toPage) => dispatch => {
+  return new Promise(resolve => {
     dispatch({
         type: C.CHANGE_PAGE,
         page: toPage
     })
-    return toPage
+    resolve(toPage)
+  })
 
 }
